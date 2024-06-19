d3a8419dfc22b8ce4e3248045ad10ced5ab95793
nicholas <nicholasobazei@gmail.com>
2024-06-19 12:23:38 +0100

    page update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,9 +2,8 @@ import "./portfolio.scss"
 import React, { useRef } from 'react';
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
-
 import unoTelos from '../../../public/unoTelos.png'
-
+import gpt3 from '../../../public/gpt3.webp'
 
 const items = [
     {
@@ -12,24 +11,28 @@ const items = [
         title: "Uno Telos",
         img: unoTelos, 
         desc: "A full website design and development of a multi-national cutting-edge technology and service company",
+        link: "https://uno-telos-project.vercel.app/",
     },
     {
         id:2,
-        title: "GPT 3",
-        img: "", 
-        desc: "A full website design and development of a multi-national cutting-edge technology and service company",
+        title: "GPT 3 React",
+        img: gpt3, 
+        desc: "An innovative and modern website design showcasing the capabilities of GPT-3. Featuring a sleek, minimalist interface",
+        link: "https://what-gpt3.vercel.app/",
     },
     {
         id:3,
         title: "Luxury Hotel",
         img: "", 
         desc: "A full website design and development of a multi-national cutting-edge technology and service company",
+        link: "",
     },
     {
         id:4,
         title: "Dating App",
         img: "", 
         desc: "A full website design and development of a multi-national cutting-edge technology and service company",
+        link: "",
     },
 ];
 
@@ -42,8 +45,16 @@ const Single = ({ item }) => {
 
     const y = useTransform(scrollYProgress, [0, 1], [-200, 200]);
 
+    const handleButtonClick = () => {
+        if (item.link) {
+            window.open(item.link, '_blank');
+        } else {
+            alert("Link not available");
+        }
+    };
+
     return ( 
-        <section >
+        <section>
             <div className="container">
                 <div className="wrapper">
                     <div className="imageContainer" ref={ref}>
@@ -52,7 +63,7 @@ const Single = ({ item }) => {
                     <motion.div className="textContainer" style={{y}}>
                         <h2>{item.title}</h2>
                         <p>{item.desc}</p>
-                        <button>See Demo</button>
+                        <button onClick={handleButtonClick}>See Demo</button>
                     </motion.div>
                 </div>
             </div>
